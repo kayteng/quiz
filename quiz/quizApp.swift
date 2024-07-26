0f2826c15416ebb5ef72c44aa78f8d3525cc3730
kayteng <kaylateng@gmail.com>
2024-07-26 11:25:21 -0700

    Initial Commit

---
+++ quiz/quizApp.swift
@@ -0,0 +1,17 @@
+//
+//  quizApp.swift
+//  quiz
+//
+//  Created by Scholar on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct quizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
